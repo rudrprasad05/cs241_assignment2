13e0554bd6ee6a32877d828e767ddab9183ad5d1
D4NSY <danifiji@gmail.com>
2024-10-14 13:27:02 +1200

    init

--- src/main/java/com/group6/assignment2/Assignment2Application.java
+++ src/main/java/com/group6/assignment2/Assignment2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Assignment2Application {
 
+	// added commeny
 	public static void main(String[] args) {
 		MySQLConnectionTest mySQLConnectionTest = new MySQLConnectionTest();
 		if(mySQLConnectionTest.test()){
