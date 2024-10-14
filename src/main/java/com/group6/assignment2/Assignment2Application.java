1bebb2e96f7a57451e832f5d4427f414fc2e1cbe
Rudr <rudrprasad@yahoo.com>
2024-10-14 13:34:22 +1200

    added mysql check on start up

--- src/main/java/com/group6/assignment2/Assignment2Application.java
+++ src/main/java/com/group6/assignment2/Assignment2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Assignment2Application {
 
+
 	public static void main(String[] args) {
 		MySQLConnectionTest mySQLConnectionTest = new MySQLConnectionTest();
 		if(mySQLConnectionTest.test()){
