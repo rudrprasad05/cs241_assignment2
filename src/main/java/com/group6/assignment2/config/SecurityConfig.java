29f7930bd2d3fd36976d718799dbe307a2bf8186
D4NSY <danifiji@gmail.com>
2024-10-17 18:52:10 +1200

    test

--- src/main/java/com/group6/assignment2/config/SecurityConfig.java
+++ src/main/java/com/group6/assignment2/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+//test
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
