d62bd3c5ce0358924a85e46ed7ed9368e493b04c
D4NSY <danifiji@gmail.com>
2024-10-17 18:56:44 +1200

    test

--- src/main/java/com/group6/assignment2/config/SecurityConfig.java
+++ src/main/java/com/group6/assignment2/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-//test brackh
+//test branch
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
